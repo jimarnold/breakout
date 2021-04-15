7ace8d662dc73795c9ee8f7e86c34dc5e126d54d
Jim Arnold <jarnold@etsy.com>
2021-04-14 22:33:11 -0400

    vectors instead of points; extracted classes for game entities

--- Game.scala
+++ Game.scala
@@ -10,129 +10,150 @@ import SDLImplicits._
 
 import scala.collection.mutable.ArrayBuffer
 
-final case class Point(x: Int, y: Int) {
-  def -(other: Point): Point = Point(this.x - other.x, this.y - other.y)
-  def +(other: Point): Point = Point(this.x + other.x, this.y + other.y)
+final case class Rect(x: Float, y: Float, width: Float, height: Float) {
+
 }
 
-final case class Rect(x: Int, y: Int, width: Int, height: Int) {
-  def contains(point: Point): Boolean = {
-    (point.x >= this.x && point.x <= (this.x + this.width)) && (point.y >= this.y && point.y <= (this.y + this.height))
+class Ball(var position: Vector, var velocity: Vector) {
+  val width = 5
+  val height = 5
+  def update(elapsed: Float): Unit = {
+    position = position.plus(velocity.mult(elapsed))
+    if (position.x < 0) {
+      position = Vector(0, position.y)
+      velocity = Vector(-velocity.x, velocity.y)
+    }
+    if (position.y < 0) {
+      position = Vector(position.x, 0)
+      velocity = Vector(velocity.x, -velocity.y)
+    }
+    if (position.x > 799) {
+      position = Vector(799, position.y)
+      velocity = Vector(-velocity.x, velocity.y)
+    }
+    if (position.y > 799) {
+      position = Vector(position.x, 799)
+      velocity = Vector(velocity.x, -velocity.y)
+    }
+  }
+
+  def draw(canvas: Canvas): Unit = {
+    canvas.setColor(255, 0, 255)
+    canvas.drawRect(position, 5, 5)
+  }
+
+  def bounds(): Rect = {
+    Rect(position.x, position.y, width, height)
+  }
+}
+
+class Brick(var position: Vector) {
+  val width = 30
+  val height = 10
+  def contains(v: Vector): Boolean = {
+    (v.x >= position.x && v.x <= (position.x + width)) && (v.y >= position.y && v.y <= (position.y + height))
   }
   def contains(rect: Rect): Boolean = {
-    rect.x < this.x + this.width &&
-    rect.x + rect.width > this.x &&
-    rect.y < this.y + this.height &&
-    rect.y + rect.height > this.y
+    rect.x < position.x + width &&
+      rect.x + rect.width > position.x &&
+      rect.y < position.y + height &&
+      rect.y + rect.height > position.y
   }
-  def +(point: Point): Rect = Rect(this.x + point.x, this.y + point.y, this.width, this.height)
-  def -(point: Point): Rect = Rect(this.x - point.x, this.y - point.y, this.width, this.height)
+  def update(): Unit = {
+
+  }
+  def draw(canvas: Canvas): Unit = {
+    canvas.setColor(0, 150, 0)
+    canvas.drawRect(position, 30, 10)
+  }
 }
 
-object Game extends App {
-  private val rand                    = new java.util.Random
-  private val title  = c"Game"
-  private val width  = 800
-  private val height = 800
-  private val Left   = Point(10, 0)
-  private val Right  = Point(-10, 0)
-  private val Stop   = Point(0, 0)
+class Paddle(var position: Vector) {
 
-  private var running                 = true
-  private var window: Ptr[Window]     = _
-  private var renderer: Ptr[Renderer] = _
-  private var bricks: List[Rect] = _
-  private var paddle: Rect           = Rect(30, 30, 60, 20)
-  private var ball: Rect             = Rect(40, 20, 5, 5)
-  private var ballVelocity: Point     = Point(0, 0)
-  private var pressed                 = collection.mutable.Set.empty[Keycode]
+  private val Left   = Vector(-1, 0)
+  private val Right  = Vector(1, 0)
+  private val Stop   = Vector(0, 0)
 
+  def update(elapsed: Float, pressed: scala.collection.Set[Keycode]): Unit = {
+    val direction =
+      if (pressed.contains(LEFT_KEY)) Left
+      else if (pressed.contains(RIGHT_KEY)) Right
+      else Stop
+    val oldPos = position
+    position = position.plus(direction.mult(elapsed * 0.5f))
+    if (position.x < 0 || position.y < 0 || position.x > 799 || position.y > 799) {
+      position = oldPos
+    }
+  }
+  def draw(canvas: Canvas): Unit = {
+    canvas.setColor(200, 200, 200)
+    canvas.drawRect(position, 60, 20)
+  }
+}
+
+case class Canvas(renderer: Ptr[Renderer]) {
   def setColor(r: Int, g: Int, b: Int): Unit =
     SDL_SetRenderDrawColor(renderer, r.toUByte, g.toUByte, b.toUByte, 0.toUByte)
   def clear(): Unit =
     SDL_RenderClear(renderer)
   def present(): Unit =
     SDL_RenderPresent(renderer)
-  def drawPoint(xy: Point, w: Int, h: Int): Unit = {
-    val rect = stackalloc[SDL.Rect].init(xy.x, xy.y, w, h)
-    SDL_RenderFillRect(renderer, rect)
-  }
-
-  def drawRect(rect: Rect): Unit = {
-    val sdlRect = stackalloc[SDL.Rect].init(rect.x, rect.y, rect.width, rect.height)
+  def drawRect(position: Vector, width: Int, height: Int): Unit = {
+    val sdlRect = stackalloc[SDL.Rect].init(position.x.toInt, position.y.toInt, width, height)
     SDL_RenderFillRect(renderer, sdlRect)
   }
+}
+
+object Game extends App {
+  private val rand   = new java.util.Random
+  private val title  = c"Game"
+  private val width  = 800
+  private val height = 800
+
+  private var running                 = true
+  private var window: Ptr[Window]     = _
+  private var renderer: Ptr[Renderer] = _
+  private var bricks: List[Brick]     = _
+  private var paddle: Paddle          = _
+  private var ball: Ball              = _
+  private var pressed                 = collection.mutable.Set.empty[Keycode]
+  private var canvas: Canvas          = _
+  private var lastTick: Long          = 0
 
   def drawBricks(): Unit = {
-    setColor(0, 150, 0)
-    bricks.foreach(brick => drawRect(brick))
   }
-  def drawPaddle(): Unit = {
-    setColor(200, 200, 200)
-    drawRect(paddle)
-  }
-  def drawBall(): Unit = {
-    setColor(255, 0, 255)
-    drawRect(ball)
-  }
+
   def onDraw(): Unit = {
-    setColor(0, 0, 0)
-    clear()
-    drawBricks()
-    drawPaddle()
-    drawBall()
-    present()
-  }
-
-  def movePaddle(newPos: Point): Unit = {
-    val oldPos = paddle
-    paddle = paddle + newPos
-    if (paddle.x < 0 || paddle.y < 0 || paddle.x > 799 || paddle.y > 799) {
-      paddle = oldPos
-    }
-  }
-
-  def moveBall(): Unit = {
-    ball = ball + ballVelocity
-    if (ball.x < 0) {
-      ball = Rect(0, ball.y, ball.width, ball.height)
-      ballVelocity = Point(-ballVelocity.x, ballVelocity.y)
-    }
-    if (ball.y < 0) {
-      ball = Rect(ball.x, 0, ball.width, ball.height)
-      ballVelocity = Point(ballVelocity.x, -ballVelocity.y)
-    }
-    if (ball.x > 799) {
-      ball = Rect(799, ball.y, ball.width, ball.height)
-      ballVelocity = Point(-ballVelocity.x, ballVelocity.y)
-    }
-    if (ball.y > 799) {
-      ball = Rect(ball.x, 799, ball.width, ball.height)
-      ballVelocity = Point(ballVelocity.x, -ballVelocity.y)
-    }
+    canvas.setColor(0, 0, 0)
+    canvas.clear()
+    bricks.foreach(brick => brick.draw(canvas))
+    paddle.draw(canvas)
+    ball.draw(canvas)
+    canvas.present()
   }
 
   def hitTest(): Unit = {
-    val toRemove = ArrayBuffer.empty[Rect]
-    bricks.foreach(brick => if (brick.contains(ball)) toRemove += brick)
+    val toRemove = ArrayBuffer.empty[Brick]
+    bricks.foreach(brick => {
+      if (brick.contains(ball.bounds())) {
+        toRemove += brick
+      }
+    })
     bricks = bricks diff toRemove.distinct
   }
 
   def keyPressed(key: Keycode): Boolean =
     pressed.contains(key)
 
-  def onIdle(): Unit = {
+  def onIdle(elapsed: Float): Unit = {
     if (keyPressed(ESCAPE)) {
       running = false
     }
     if (keyPressed(SPACE)) {
       newGame()
     } else {
-      movePaddle(
-        if (keyPressed(LEFT_KEY)) Left
-        else if (keyPressed(RIGHT_KEY)) Right
-        else Stop)
-      moveBall()
+      paddle.update(elapsed, pressed)
+      ball.update(elapsed)
       hitTest()
     }
   }
@@ -142,22 +163,24 @@ object Game extends App {
     SDL_Init(INIT_VIDEO)
     window = SDL_CreateWindow(title, 0, 0, width, height, WINDOW_SHOWN)
     renderer = SDL_CreateRenderer(window, -1, VSYNC)
+    canvas = Canvas(renderer)
+    lastTick = System.nanoTime()
     newGame()
   }
 
   def newGame(): Unit = {
-    bricks = (0 to 19).map { e => Rect(e * 40, 10, 30, 10) }.toList
-    paddle = Rect(400, 700, 60, 20)
-    ball = Rect(410, 695, 5, 5)
-    val r1 = 1 + rand.nextInt(( 8 - 1) + 1)
-    val r2 = 1 + rand.nextInt(( 8 - 1) + 1)
-    ballVelocity = Point(r1, r2)
+    bricks = (0 to 19).map { e => new Brick(Vector(e * 40, 10)) }.toList
+    paddle = new Paddle(Vector(400, 700))
+    val r1 = (0.5f - rand.nextFloat()) * 0.1f
+    val r2 = -0.1f
+    ball = new Ball(Vector(410, 695), Vector(r1, r2))
   }
 
   def delay(ms: UInt): Unit =
     SDL_Delay(ms)
 
   def loop(): Unit = {
+    val elapsed = (System.nanoTime() - lastTick).toFloat / 1000
     val event = stackalloc[Event]
     while (running) {
       while (SDL_PollEvent(event) != 0) {
@@ -173,7 +196,7 @@ object Game extends App {
         }
       }
       onDraw()
-      onIdle()
+      onIdle(elapsed)
       delay(20.toUInt)
     }
   }
---
+++ Vector.scala
@@ -0,0 +1,44 @@
+package breakout
+
+import scala.math.sqrt
+
+case class Vector(x: Float, y: Float) {
+  def magnitude(): Float = {
+    sqrt((this.x * this.x) + (this.y + this.y)).toFloat
+  }
+
+  def mult(b: Float): Vector = {
+    Vector(this.x * b, this.y * b)
+  }
+
+  def divide(m: Float): Vector = {
+    Vector(this.x / m, this.y / m)
+  }
+
+  def plus(v: Vector): Vector = {
+    Vector(this.x + v.x, this.y + v.y)
+  }
+
+  def minus(v: Vector): Vector = {
+    Vector(this.x - v.x, this.y - v.y)
+  }
+
+  def normalize(): Vector = {
+    val m = this.magnitude()
+    if (m > 0) this.divide(m) else Vector(0, 0)
+  }
+
+  def dot(v: Vector): Float = {
+    this.x * v.x + this.y * v.y
+  }
+
+  def reflect(n: Vector): Vector = {
+    this.minus(n.mult(2 * this.dot(n)))
+  }
+
+  def project(onto: Vector): Vector = {
+    val numerator = this.dot(onto)
+    val denominator = onto.dot(onto)
+    onto.mult(numerator / denominator)
+  }
+}
